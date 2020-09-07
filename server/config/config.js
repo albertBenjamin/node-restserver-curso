3b111649bc4daa4733dc60d4e69fd0ae520cab52
abmunozp <abmunoz@entel.cl>
2020-09-07 16:42:57 -0300

    variable entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://albert:2S7zrc5F2mkkWf4c@cluster0.9xw5e.mongodb.net/cafe';
+    urlBD = process.env.MONGO_UIR;
 }
 
 process.env.URLDB = urlBD;
